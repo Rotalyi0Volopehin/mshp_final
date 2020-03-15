adb894bc52ad0dbded69bb241dc56b28c75e0438
Andrey Bolnykh <astupidface12@gmail.com>
2020-03-15 15:48:41 +0300

    nstruction

--- src/desktop/scenes/map.py
+++ src/desktop/scenes/map.py
@@ -2,11 +2,18 @@ from network_confrontation.src.desktop.constants import Color
 from network_confrontation.src.desktop.objects.button import Btn
 from network_confrontation.src.desktop.scenes.base import Scene
 from network_confrontation.src.desktop.objects.hex_grid import Grid
+from network_confrontation.src.desktop.objects.text import Text
 
 class MapScene(Scene):
     def create_objects(self):
         self.map = Grid(self.game,20,8,8,2000)
-        self.objects = [self.map]
+
+        self.instruction = Text(self.game,x=50,y=300, text="Инструкция"
+                                                           "1 Выбери клетку кликом мыши"
+                                                           "2 Зеленые клетки - ")
+
+        self.objects = [self.map,self.instruction]
+
 
     def set_main_scene(self):
         self.set_next_scene(self.game.MAIN_SCENE_INDEX)
