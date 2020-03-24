020cb506f6f9811b3b226283e8fa62b78dbf5f53
Melyohin.AA <melyohin.aa@gmail.com>
2020-03-24 15:43:25 +0300

    Improvement of 'error_response' module

--- src/core/net_connection/response_ids.py
+++ src/core/net_connection/response_ids.py
@@ -5,7 +5,7 @@ from enum import Enum
 
 
 class ResponseID(Enum):
-    SUCCESS = 0  # parcel[1] { response_id<int> }
-    FAIL = 1  # parcel[1] { response_id<int> }
-    ERROR = 2  # parcel[2] { response_id<int>, error_id<int> }
-    DATA = 3  # parcel[2] { response_id<int>, data<object> }
+    SUCCESS = 0  # parcel[1] { response_id<ResponseID> }
+    FAIL = 1  # parcel[1] { response_id<ResponseID> }
+    ERROR = 2  # parcel[2] { response_id<ResponseID>, error_id<ErrorResponseID> }
+    DATA = 3  # parcel[2] { response_id<ResponseID>, data<object> }
