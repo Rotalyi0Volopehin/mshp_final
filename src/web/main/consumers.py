ec2a0d090a918dcaac8d667c9cb2e1a548487b2b
Melyohin.AA <melyohin.aa@gmail.com>
2020-03-24 15:29:26 +0300

    Websocket request format checker was added

--- src/web/main/consumers.py
+++ src/web/main/consumers.py
@@ -1,4 +1,11 @@
+import json
+import net_connection.error_response as err_resp
+
 from channels.generic.websocket import WebsocketConsumer
+from net_connection.json_serialize import CoreJSONDecoder
+
+
+# TODO: задокументировать
 
 
 class WebsocketRequestHandler(WebsocketConsumer):
@@ -9,9 +16,36 @@ class WebsocketRequestHandler(WebsocketConsumer):
         pass
 
     def receive(self, text_data=None, bytes_data=None):
-        # echo
-        if text_data is not None:
-            text_data = text_data[::-1]
+        # vvv первоначальная проверка формата request-а vvv
+        if not self.check_bytes_data_absence(bytes_data):
+            return
+        ok, parcel = self.try_parse_json_into_parcel(text_data)
+        if not ok:
+            return
+        if not self.check_parcel_format(parcel):
+            return
+        # vvv делегирование к обработчикам request-ов в соответствии с id request-ов vvv
+        pass
+
+    def check_bytes_data_absence(self, bytes_data) -> bool:
         if bytes_data is not None:
-            bytes_data = bytes_data[::-1]
-        self.send(text_data=text_data, bytes_data=bytes_data)
+            error_response = err_resp.compose_error_response_parcel(err_resp.ErrorResponseID.BYTE_FORMAT_NOT_SUPPORTED)
+            self.send(json.dumps(error_response))
+            return False
+        return True
+
+    def try_parse_json_into_parcel(self, possible_json) -> (bool, object):
+        try:
+            parcel = CoreJSONDecoder.decode_json(possible_json)
+            return True, parcel
+        except:
+            error_response = err_resp.compose_error_response_parcel(err_resp.ErrorResponseID.JSON_FORMAT_REQUIRED)
+            self.send(error_response)
+            return False, None
+
+    def check_parcel_format(self, parcel) -> bool:
+        if not (isinstance(parcel, list) and (len(parcel) > 0) and isinstance(parcel[0], err_resp.ErrorResponseID)):
+            error_response = err_resp.compose_error_response_parcel(err_resp.ErrorResponseID.WRONG_PARCEL_FORMAT)
+            self.send(error_response)
+            return False
+        return True
