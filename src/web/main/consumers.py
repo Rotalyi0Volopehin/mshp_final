fa7690f605ec6badbd79ddbb0dc72aeb1a7a53d4
Melyohin.AA <melyohin.aa@gmail.com>
2020-03-24 13:32:56 +0300

    Base of websocket request processing system

--- src/web/main/consumers.py
+++ src/web/main/consumers.py
@@ -1,12 +1,17 @@
 from channels.generic.websocket import WebsocketConsumer
 
 
-class EchoConsumer(WebsocketConsumer):
+class WebsocketRequestHandler(WebsocketConsumer):
     def connect(self):
         self.accept()
 
     def disconnect(self, code):
         pass
 
-    def receive(self, text_data):
-        self.send(text_data)
+    def receive(self, text_data=None, bytes_data=None):
+        # echo
+        if text_data is not None:
+            text_data = text_data[::-1]
+        if bytes_data is not None:
+            bytes_data = bytes_data[::-1]
+        self.send(text_data=text_data, bytes_data=bytes_data)
