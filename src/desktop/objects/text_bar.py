b7366abc49640887016b7a645884a5b5133086bd
Sharonov Aleksey <aleksey.sharonov@gmail.com>
2020-04-11 16:18:47 +0300

    add perehod mejdu failami

--- src/desktop/objects/text_bar.py
+++ src/desktop/objects/text_bar.py
@@ -4,11 +4,13 @@ from objects.base import DrawObject
 
 
 class TextBar(DrawObject):
-    def __init__(self, game, file_name, font_name='Comic Sans', font_size=24, is_bold=True,
-                 is_italic=False, text='Define me!',
-                 color=(255, 255, 255), x=10, y=350, width=780, height=240):
+    def __init__(self, game, file_name, path_to_file, font_name='Comic Sans', font_size=24, is_bold=True,
+                 is_italic=False, color=(255, 255, 255), x=10, y=350, width=780, height=240):
         super().__init__(game)
-        self.text_index = 0
+        self.dialog_index = 0
+        self.end_quest = False
+        self.path_to_file = path_to_file
+        self.file_name = file_name[0:len(file_name) - 1]
         self.font_name = font_name
         self.font_size = font_size
         self.is_bold = is_bold
@@ -18,7 +20,7 @@ class TextBar(DrawObject):
         self.y = y
         self.width = width
         self.height = height
-        self.data = text
+        self.data = self.get_data_ff(self.path_to_file, self.file_name, self.dialog_index)
         self.now_word_a = 0
         self.clicked = False
         self.text_frame = ''
@@ -34,23 +36,22 @@ class TextBar(DrawObject):
 
     def process_logic(self):
         self.set_next_dialog()
-        #self.set_next_ev()
+        self.set_next_ev()
 
     def set_next_dialog(self):
         if self.is_start:
             self.lal = 0
-            self.next = False
             self.is_start = False
             self.count = 0
             self.data_strings = []
             self.text_frame = ''
             self.text_frame = self.data[self.data.find('<d>', self.now_word_a) + 3:
-                                            self.data.find('</d>', self.now_word_a)]
+                                        self.data.find('</d>', self.now_word_a)]
             self.flag = True
             self.now_word_f = 0
             while self.flag:
                 self.data_strings.append(self.text_frame[self.text_frame.find('<p>', self.now_word_f) + 3:
-                                            self.text_frame.find('</p>', self.now_word_f)])
+                                                         self.text_frame.find('</p>', self.now_word_f)])
                 self.now_word_f = self.text_frame.find('</p>', self.now_word_f) + 1
                 if self.text_frame.find('</p>', self.now_word_f) == -1:
                     self.flag = False
@@ -58,34 +59,32 @@ class TextBar(DrawObject):
 
         if self.data.find('</d>', self.now_word_a) < 0 and self.lal + 1 == len(self.data_strings) and not self.end:
             self.end = True
-            print("ASD")
-            print(self.lal)
-
-
-
 
     def set_next_ev(self):
         if self.end:
-            self.text_index += 1
+            self.dialog_index = 1
+            self.data = self.get_data_ff(self.path_to_file, self.file_name, self.dialog_index)
+            # TODO Если файлов
+            # больше нет,то перейти в меню
             self.now_word_a = 0
+            self.lal = 0
             self.text_frame = ''
-            self.data_strings = []
+            self.data_strings = ['v']
             self.flag = True
-            self.count = 0
+            self.is_start = True
             self.end = False
-
+            print(self.data)
 
     def process_draw(self):
         pygame.draw.rect(self.game.screen, Color.GREY_BLUE, (self.x, self.y, self.width, self.height))
         pygame.draw.rect(self.game.screen, Color.GREEN, (self.x, self.y, self.width, self.height), 2)
         pygame.draw.rect(self.game.screen, Color.RED, (self.x + 5, self.y + 5, self.width - 10, self.height - 10), 2)
-        #for item in self.data_strings:
+        # for item in self.data_strings:
         self.count = 0
-        for i in range(self.lal+1):
+        for i in range(self.lal + 1):
             text_surface = self.font.render(self.data_strings[i], True, self.color)
             self.game.screen.blit(text_surface, [self.x + 10, self.y + 10 + self.count])
             self.count += self.font_size
-
 
     def process_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
@@ -102,6 +101,11 @@ class TextBar(DrawObject):
             if self.lal + 1 >= len(self.data_strings):
                 self.is_start = True
             else:
-                self.next = True
                 self.lal += 1
         self.clicked = False
+
+    def get_data_ff(self, a, b, c):
+        file = open(a + b + str(c), 'r')
+        stri = file.read()
+        file.close()
+        return stri
--- src/desktop/scenes/quest.py
+++ src/desktop/scenes/quest.py
@@ -9,10 +9,7 @@ from scenes.base import Scene
 class QuestScene(Scene):
 
     def create_objects(self):
-        file = open('quests/quest_1/text_0', 'r')
-        string = file.read()
-        file.close()
-        self.text_bar = TextBar(self.game, file_name='quests/quest_1/', text=string)
+        self.text_bar = TextBar(self.game, file_name='text_0', path_to_file='quests/quest_1/')
         self.image_char = Image(self.game, file_name='images/cyber.png', x=100, y=300)
         self.button_back = Btn(self.game, (350, 500, 100, 40), Color.WHITE, "Меню", self.back_to_menu)
         self.objects = [self.button_back, self.image_char, self.text_bar]
