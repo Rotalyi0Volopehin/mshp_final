4c20fa924335fe81b3e18a8fe1944cc7d3b3c830
Andrew Smirnov <smirnov@informatics.ru>
2020-05-03 20:24:13 +0300

    Prepare for production

---
+++ project/docker/run.sh
@@ -0,0 +1,5 @@
+cd /network_confrontation/src/web || exit 1
+python manage.py migrate
+python manage.py collectstatic --noinput
+daphne -b 0.0.0.0 -p 8085 --access-log /var/log/daphne/ms103_nc/daphne_access.log network_confrontation_web.asgi:application
+
