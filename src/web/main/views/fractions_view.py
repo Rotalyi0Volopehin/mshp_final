8b57396fc267443c384f1c8692404d7107ee0d96
Sergey Petrovskiy <silense_please@mail.ru>
2020-05-03 22:22:04 +0300

    duplicate code deleted, fraction names displayed at profile, progressbar hardcode removed

--- src/web/main/views/fractions_view.py
+++ src/web/main/views/fractions_view.py
@@ -13,7 +13,7 @@ class FractionPages(View):
     def sort_members_by_reputation(team: int):
         members = {}
         for user in User.objects.all():
-            if DBUserTools.try_get_user_data(user) != None:
+            if DBUserTools.try_get_user_data(user) != None and not User.is_superuser:
                 user_data, error = DBUserTools.try_get_user_data(user)
                 if user_data.team == team:
                     members[user.username] = user_data.reputation
@@ -33,18 +33,18 @@ class FractionPages(View):
         context = FractionPages.collect_default_context(request)
         context['pagename'] = 'Cyber Corp'
         context['members'] = FractionPages.sort_members_by_reputation(0)
-        return render(request, 'pages/fractions/fraction1.html', context)
+        return render(request, 'pages/fractions/fraction0.html', context)
 
     @staticmethod
     def fraction2_page(request):
         context = FractionPages.collect_default_context(request)
         context['pagename'] = 'Underground'
         context['members'] = FractionPages.sort_members_by_reputation(1)
-        return render(request, 'pages/fractions/fraction2.html', context)
+        return render(request, 'pages/fractions/fraction1.html', context)
 
     @staticmethod
     def fraction3_page(request):
         context = FractionPages.collect_default_context(request)
         context['pagename'] = 'Freedom call'
         context['members'] = FractionPages.sort_members_by_reputation(2)
-        return render(request, 'pages/fractions/fraction3.html', context)
+        return render(request, 'pages/fractions/fraction2.html', context)
