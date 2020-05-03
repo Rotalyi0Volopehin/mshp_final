c0cbcd7f46a6fd67f9cb70afa4d387e3ad7e701a
Andrew Smirnov <smirnov@informatics.ru>
2020-05-03 20:52:23 +0300

    Fix production settings

--- project/docker/run.sh
+++ project/docker/run.sh
@@ -1,5 +1,4 @@
 cd /network_confrontation/src/web || exit 1
 python manage.py migrate
 python manage.py collectstatic --noinput
-daphne -b 0.0.0.0 -p 8085 --access-log /var/log/daphne/ms103_nc/daphne_access.log network_confrontation_web.asgi:application
-
+daphne -b 0.0.0.0 -p 8085 --access-log /var/log/ms103_nc/daphne_access.log network_confrontation_web.asgi:application
