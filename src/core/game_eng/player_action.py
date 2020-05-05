6498a9e41b9d43e004c44f2f70decb0a71ce110d
Melyohin.AA <melyohin.aa@gmail.com>
2020-05-05 14:51:08 +0300

    Merge with branch 'core/feature/game_eng_ext'

---
+++ src/core/game_eng/player_action.py
@@ -0,0 +1,30 @@
+import exceptions
+
+from game_eng.player import Player
+from game_eng.grid_tile import GridTile
+
+
+class PlayerAction:
+    """**Модель действия, совершаемого игроком**\n
+    abstract class\n
+    Этот класс необходим для журналирования ходов.
+    """
+    def __init__(self, player: Player, target: GridTile):
+        if not isinstance(player, Player):
+            raise exceptions.ArgumentTypeException()
+        self.player = player
+        self.target = target
+
+    def try_do(self) -> bool:
+        """**Попытка совершить действие**\n
+        :return: ok
+        :rtype: bool
+        """
+        raise exceptions.NotImplementedException()
+
+    def try_undo(self) -> bool:
+        """**Попытка отменить действие**\n
+        :return: ok
+        :rtype: bool
+        """
+        raise exceptions.NotImplementedException()
