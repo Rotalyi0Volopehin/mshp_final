e0ad71dda2f9710bdfa87f79e913b74fb3396006
Melyohin.AA <melyohin.aa@gmail.com>
2020-05-24 17:11:23 +0300

    'GET_PARTICIPATION_STATUS' request id was added

--- src/core/net_connection/request_ids.py
+++ src/core/net_connection/request_ids.py
@@ -1,9 +1,7 @@
 from enum import Enum
 
 
-# TODO: задокументировать
-
-
 class RequestID(Enum):
     LOGIN = 0  # parcel[3] { request_id<RequestID>, login<str>, password<str> }
     LOGOUT = 1  # parcel[1] { request_id<RequestID> }
+    GET_PARTICIPATION_STATUS = 2  # parcel[1] { request_id<RequestID> }
