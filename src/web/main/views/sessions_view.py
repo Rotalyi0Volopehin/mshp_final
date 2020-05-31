42b784eb09eb96fe45eb0f0d2a56dd44596f0405
Melyohin.AA <melyohin.aa@gmail.com>
2020-05-31 19:19:45 +0300

    Backend of 'sessions' page was completed

--- src/web/main/views/sessions_view.py
+++ src/web/main/views/sessions_view.py
@@ -2,8 +2,10 @@ import main.forms as forms
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from main.models import UserParticipation
+from main.models import UserParticipation, GameSession
+from main.db_tools.user_tools import DBUserTools
 from main.db_tools.user_participation_tools import DBUSerParticipationTools
+from main.db_tools.game_session_tools import DBGameSessionTools
 from main.views.form_view import FormView
 from main.views.menu import get_user_menu_context
 
@@ -35,12 +37,7 @@ class SessionsFormPage(FormView):
         page_count = len(sessions) // SessionsFormPage.ROWS_PER_PAGE
         if len(sessions) % SessionsFormPage.ROWS_PER_PAGE > 0:
             page_count += 1
-        context["cant_back"] = page_ind <= 0
-        context["cant_forward"] = page_ind >= page_count
-        context["page"] = page_ind + 1
-        context["prev_page"] = page_ind - 1
-        context["next_page"] = page_ind + 1
-        context["page_count"] = page_count
+        SessionsFormPage.__put_page_info_into_context(context, page_ind, page_count)
         context["table"] = table = list()
         for i in range(SessionsFormPage.ROWS_PER_PAGE):
             if i == len(sessions):
@@ -60,3 +57,45 @@ class SessionsFormPage(FormView):
         participant_required = session.user_per_team_count * 3
         player_count = f"{participant_count} из {participant_required}"
         return session, level_limits, player_count
+
+    @staticmethod
+    def __put_page_info_into_context(context: dict, page_ind: int, page_count: int):
+        context["nothing_found"] = page_count == 0
+        context["cant_back"] = page_ind <= 0
+        context["cant_forward"] = page_ind >= page_count - 1
+        context["page"] = page_ind + 1
+        context["prev_page"] = page_ind - 1
+        context["next_page"] = page_ind + 1
+        context["page_count"] = page_count
+
+    @staticmethod
+    def post_handler(context: dict, request, form):
+        """**Дополнительный обработчик post-запросов**\n
+        Вызывается методом :meth:`main.views.form_view.FormView.post`
+
+        :param context: контекст страницы
+        :type context: dict
+        :param request: запрос на страницу '/sessions/'
+        :type request: HttpRequest
+        :param form: форма, содержащая post-данные
+        :type form: SessionsForm
+        """
+        session_title = form.data["session_title"]
+        if len(session_title) == 0:
+            SessionsFormPage.get_handler(context, request)
+        session = GameSession.objects.filter(title=session_title)
+        session = None if len(session) == 0 else session[0]
+        context["table"] = table = list()
+        page_count = 0
+        if session is not None:
+            user_data, error = DBUserTools.try_get_user_data(request.user)
+            if error is not None:
+                raise Exception(error)
+            participation_possibility, error =\
+                DBGameSessionTools.can_user_take_part_in_session(request.user, user_data, session)
+            if error is not None:
+                raise Exception(error)
+            if participation_possibility:
+                table.append(SessionsFormPage.__get_session_table_row(session))
+                page_count = 1
+        SessionsFormPage.__put_page_info_into_context(context, 0, page_count)
