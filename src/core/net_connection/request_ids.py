7a7b74f8da55cae885900e2b68d4a93c96fa7fa3
Melyohin.AA <melyohin.aa@gmail.com>
2020-06-02 11:27:39 +0300

    'GET_GAME' RequestID was added

--- src/core/net_connection/request_ids.py
+++ src/core/net_connection/request_ids.py
@@ -5,3 +5,4 @@ class RequestID(Enum):
     LOGIN = 0  # parcel[3] { request_id<RequestID>, login<str>, password<str> }
     LOGOUT = 1  # parcel[1] { request_id<RequestID> }
     GET_GS_INFO = 2  # parcel[1] { request_id<RequestID> }
+    GET_GAME_MODEL = 3  # parcel[1] { request_id<RequestID> }
