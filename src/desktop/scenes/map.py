dd561e53197703ffccc2c87dc793063478bb36ec
Andrey Bolnykh <astupidface12@gmail.com>
2020-06-05 18:24:32 +0300

    bug fix

--- src/desktop/scenes/map.py
+++ src/desktop/scenes/map.py
@@ -5,11 +5,15 @@ from objects.toolbar import ToolBar
 from objects.end_turn_button import EndTurnButton
 from objects.current_player_plate import CurrentPlayerPlate
 from objects.grid_tile_info_plate import GridTileInfoPlate
-from scenes.menu import MenuScene
+from scenes.main_menu import MainMenuScene
 from scenes.tech_tree import TreeScene
 
 
 class MapScene(Scene):
+    def __init__(self, game):
+        super().__init__(game)
+
+
     def create_objects(self):
         width = self.game.width
         height = self.game.height
@@ -28,3 +32,9 @@ class MapScene(Scene):
         self.objects.append(current_player_plate)
         grid_tile_info_plate = GridTileInfoPlate(self.game, width - 20, 50, 340)
         self.objects.append(grid_tile_info_plate)
+
+    def __set_menu_scene(self):
+        self.game.goto_deeper_scene(MainMenuScene)
+
+    def __set_tech_scene(self):
+        self.game.goto_deeper_scene(TreeScene)
