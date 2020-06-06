ad79120e9d5ddebc5dccd70d5bb3f4c3e1d5150a
Melyohin.AA <melyohin.aa@gmail.com>
2020-06-07 00:01:48 +0300

    Fix of 'EndTurnButton' (now it is not button)

--- src/desktop/objects/end_turn_button.py
+++ src/desktop/objects/end_turn_button.py
@@ -68,6 +68,7 @@ class EndTurnButton(DrawObject):
         self.time_left_label.process_draw()
 
     def process_event(self, event):
+        return  # всё очень плохо
         if (event.type == pygame.MOUSEBUTTONUP) and (event.button == 1) and \
                 triangle_contains_point(*self.points, event.pos[0], event.pos[1]) and \
                 self.game.current_scene.game_vc.player_controller.changes_available:
