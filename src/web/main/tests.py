c5081181be99b8ba234e6c93fd124bd534ff76c9
Ksenouter <ProhorovIlyaDm@gmail.com>
2020-06-07 16:48:17 +0300

    test_auth_protection

--- src/web/main/tests.py
+++ src/web/main/tests.py
@@ -1,3 +1,21 @@
-# from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class DarkNetTests(TestCase):
+
+    def test_auth_protection(self):
+        unauth_client = Client()
+        response = unauth_client.get(reverse('darknet'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/login/?next=/darknet/')
+
+
+class SessionsTests(TestCase):
+
+    def test_auth_protection(self):
+        unauth_client = Client()
+        response = unauth_client.get(reverse('sessions'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
